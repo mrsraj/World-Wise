03ac6782c0c1c14ecf062f9bcdea9c41210e88ce
Sachin Raj <sachinraj6392@gmail.com>
2024-08-22 07:47:54 +0530

    add map

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,11 +1,31 @@
 
+import styles from "./Form.module.css";
+
+import { useNavigate } from "react-router-dom";
 
 function Form() {
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        console.log("handled");
+
+        e.preventDefault();
+        navigate(-1);
+    }
+
+    function GoPrevPage() {
+        navigate(-1);
+    }
+
     return (
         <div>
-            <form action="#">
-                <input type="text" id="name" name="name"/>
+            <form action="#" className={styles.form} onSubmit={handleSubmit}>
+                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" />
+                <button type="submit">Add</button>
             </form>
+            <button onClick={GoPrevPage}>-back</button>
         </div>
     )
 }
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -4,8 +4,13 @@ import style from '../Components/Map.module.css'
 //import hooks
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 
+//import map releted library:
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { useEffect, useState } from 'react';
+
 
 function Map() {
+  const [mapPosition, setMapPosition] = useState([25, 86]);
   const navigate = useNavigate();
 
   //code for retrive data from urls using useSearchParams
@@ -14,18 +19,25 @@ function Map() {
 
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+  // useEffect(() => {
+  //   setMapPosition([lat, lng]);
+  // }, []);
 
 
   return (
-    <div className={style.mapContainer} key={id} onClick={() => {
-      navigate("form")
-    }}>
-      <h2>Posistion</h2>
-      <h3>lat {lat}</h3>
-      <h3>lng {lng}</h3>
-      <button onClick={() => {
-        setUseParams({ lat: 20, lng: 50 });
-      }}>Change Position</button>
+    <div className={style.mapContainer} key={id} onClick={() => { navigate("form") }}>
+
+      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={style.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
